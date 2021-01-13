30df97416c6c4d40ad2a4d621bb70a07e80fec69
Alexander Dahl <post@lespocky.de>
2021-01-13 22:47:31 +0100

    gen-v6-ula: Fix EUI_48 extraction if net iface passed as option

--- gen-v6-ula.sh
+++ gen-v6-ula.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 # taken and modified from https://stackoverflow.com/a/48974449/462636
-t="$(ntptime | grep "time " | head -n1 | awk '{print $2}')"
-if [ -z "$1" ]
-then
-    if=eth0
-    EUI_48="$(ip a s dev $if | grep "link/ether" | awk '{print $2}')"
-else
-    EUI_48="$1"
-fi
+t="$(/usr/sbin/ntptime | grep "time " | head -n1 | awk '{print $2}')"
+if=${1:-eth0}
+EUI_48="$(ip a s dev $if | awk '/link\/ether/ {print $2}')"
 EUI_64="$(ipv6calc --action prefixmac2ipv6 --in prefix+mac --out ipv6addr :: $EUI_48 | sed 's/^:://')"
 conq="$(echo "$t $EUI_64" | tr -d ".: ")"
 sha1="$(for x in $(echo "$conq" | sed 's/\(..\)/\1 /g') ; do printf "\x${x}" ; done | sha1sum | awk '{print $1}' | tail -c 11 | sed 's/\(..\)/\1 /g')"
