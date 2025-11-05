cfa967af513c984139ca5396dc40406e83c80dd0
Alexander Dahl <post@lespocky.de>
2025-11-05 18:18:50 +0100

    kde-shutdown: Check for 'promptAll' method before using it
    Always want the whole list, but different KDE versions do different
    things here, so we need to check first.

--- kde-shutdown.sh
+++ kde-shutdown.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-qdbus6 org.kde.LogoutPrompt /LogoutPrompt promptShutDown
+#!/bin/bash
+set -o pipefail
+
+if qdbus6 org.kde.LogoutPrompt /LogoutPrompt | grep -q promptAll
+then
+    qdbus6 org.kde.LogoutPrompt /LogoutPrompt promptAll
+else
+    qdbus6 org.kde.LogoutPrompt /LogoutPrompt promptShutDown
+fi
